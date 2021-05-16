2c1aeacc9978c69909ce03f45ab33baf5931321b
Sahil Hadke <sahil.hadke@gmail.com>
2021-05-16 22:50:09 +0530

    added default port

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3030
 
 app.use(express.json()) // parses the body
 app.use(userRouter)
